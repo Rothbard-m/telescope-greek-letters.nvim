a66a71a3fa56a75ce7a121c80d5994b59256fbf4
Rothbard-m <owen.james.m@gmail.com>
2023-05-22 19:57:02 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# telescope-greek-letters
+
+Search greek letters using [telescope.nvim](https://github.com/nvim-telescope) and insert LaTeX code on selection.
+
+## Requirements
+
+This plugin requires [telescope.nvim](https://github.com/nvim-telescope). Please make sure you have it installed.
+
+## Installation
+
+Lazy
+
+```lua
+{
+    'Rothbard-m/telescope-greek-letters.nvim'
+        config = function()
+        require('greek_letters_plugin').setup{
+            keymap = "<leader>fl"  -- Use "<leader>fl" as the keymap
+                end,
+                dependencies = { 'nvim-telescope/telescope.nvim' }
+        }
+```
+
+## Setup
+
+To initialise the plugin after installing. Call the set up function shown below. This is a simple plugin with the only option to set a different keymap from the default.
+
+```lua
+require('greek_letters_plugin').setup{
+    keymap = "<leader>fl"  -- Use "<leader>fl" as the keymap
+}
+```
+
+## Usage
---
+++ lua/greek_letters/greek_letters.lua
@@ -0,0 +1,63 @@
+local telescope = require('telescope')
+local actions = require('telescope.actions')
+local actions_state = require('telescope.actions.state')
+local entry_display = require('telescope.pickers.entry_display')
+
+-- Define the list of Greek letters
+local greek_letters = {
+    { label = 'alpha', code = '\\alpha' },
+    { label = 'beta', code = '\\beta' },
+    { label = 'gamma', code = '\\gamma' },
+    -- Add more Greek letters as needed
+}
+
+-- Define the Telescope picker
+local greek_picker = telescope.make_entry {
+    display = entry_display.create {
+        separator = ' ',
+        items = {
+            { width = 10 },
+            { remaining = true },
+        }
+    },
+    sorter = entry_display.sorters.get_generic_fuzzy_sorter(),
+}
+
+-- Create the Telescope picker for Greek letters
+local pickers = require('telescope.pickers')
+local find_greek_letter = function(prompt_bufnr)
+    local letter = actions_state.get_selected_entry(prompt_bufnr)
+    actions.close(prompt_bufnr)
+    if letter then
+        vim.api.nvim_put({ letter.code }, '', true, true)
+    end
+end
+
+local greek_letters_picker = function()
+    pickers.new {
+        prompt_title = 'Greek Letters',
+        finder = telescope.make_builtin {
+            results = greek_letters,
+            entry_maker = function(letter)
+                return {
+                    value = letter,
+                    ordinal = letter.label,
+                    display = {
+                        letter.label,
+                        letter.code,
+                    },
+                }
+            end,
+        },
+        sorter = greek_picker,
+        attach_mappings = function(prompt_bufnr, map)
+            map('i', '<CR>', find_greek_letter)
+            map('n', '<CR>', find_greek_letter)
+            return true
+        end,
+    }:find()
+end
+
+return {
+    greek_letters_picker = greek_letters_picker
+}
